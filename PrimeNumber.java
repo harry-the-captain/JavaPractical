068fd54363b943d30abfbb67832d86b304bcb435
harry-the-captain <2022bit064@sggs.ac.in>
2024-02-13 23:06:15 +0530

    PrimeNumber

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+class PrimeNumber{
+    public static void main(String args[]){
+       int no=3;
+       int temp=0;
+       for(int i=2; i<=no-1; i++){
+             if(no%i==0){
+                temp = temp +1;
+             }
+       }
+
+       if(temp==0){
+          System.out.println(no+" Prime Number ");
+       }
+
+       else{
+          System.out.println(no+" not prime ");
+       }
+   }
+}
